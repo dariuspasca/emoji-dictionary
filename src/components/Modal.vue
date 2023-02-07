6a2e896baa5a57c26ccf973d253d9f9c31ed4576
Darius Pasca <dariuspasca@protonmail.com>
2023-02-07 15:04:53 +0100

    update: modal removed defaults

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -14,21 +14,18 @@ export default {
       class="fixed top-0 left-0 z-50 flex h-full w-full bg-zinc-900/[0.85] transition-opacity"
     >
       <div
-        class="m-auto w-10/12 rounded-md bg-zinc-800 px-6 py-4 shadow-md shadow-purple-500/[0.55] transition-all md:w-[500px]"
+        class="m-auto w-10/12 rounded-md bg-zinc-800 px-6 py-4 shadow-md shadow-purple-500/[0.55] transition-all md:mt-36 md:w-[500px]"
       >
         <div class="text-white">
-          <slot name="header">default header</slot>
+          <slot name="header"></slot>
         </div>
 
         <div class="my-4">
-          <slot name="body">default body</slot>
+          <slot name="body"></slot>
         </div>
 
         <div class="mt-4">
-          <slot name="footer">
-            default footer
-            <button class="float-right" @click="$emit('close')">OK</button>
-          </slot>
+          <slot name="footer"> </slot>
         </div>
       </div>
     </div>
